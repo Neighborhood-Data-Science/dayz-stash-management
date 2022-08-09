ad4a8bb64fe1fd3fb24c754fd64719f56fbb4ef2
DaMaddCyantist <38044300+DaMaddCyantist@users.noreply.github.com>
2022-08-08 23:57:03 -0500

    Latest update for streamlit app. Closes a few commits.

--- createNewStash.py
+++ createNewStash.py
@@ -9,11 +9,17 @@ from PIL import Image
 from io import BytesIO
 
 @st.cache
-def get_data():
+def get_image_data():
     path = r'dayz_images_info.csv'
     return pd.read_csv(path)
 
-image_info = get_data()
+@st.cache
+def get_city_names():
+    path = r'chernarus_cities.csv'
+    return pd.read_csv(path)
+
+image_info = get_image_data()
+city_names = get_city_names()
 
 
 def update_item_list():
@@ -26,35 +32,43 @@ def update_image():
     """
     Update the image based on item selection.
     """
-    image_link = image_info[image_info['item_Name']==st.session_state.item_Name].loc[:,'image_link'][0]
+    image_link = image_info[image_info['item_Name']==st.session_state.item_Name].loc[:,'image_Link'].tolist()[0]
     image_request = requests.get(image_link)
     image = Image.open(BytesIO(image_request.content))
 
 def app():
-
+    #Set variables (for now)
     container_type = ['','Crate', 'CargoTent','SeaChest','Barrell','Drybag Backpack']
-    location_name = ['','Cherno','Elektro','Tisy']
-
+    
     st.text_input('Title',placeholder="Enter a new title for your stash.")
+
     #Select container type
     select_container = st.selectbox('Stash Type',container_type, key='container_Name')
+
     #Select location name
+    location_name = city_names.loc[:,'Location'].tolist()
     select_location = st.selectbox('Location',location_name,key='location_Name')
+
     #Select category of item
     category_name = image_info.loc[:,'Category'].unique().tolist()
     select_category = st.selectbox('Category',category_name,key='item_Category',on_change=update_item_list)
+
     #Select item based on category (if checked) otherwise, return all items
     if select_category:
         item_list = image_info[image_info['Category']==select_category].loc[:,'item_Name'].tolist()
         select_item = st.selectbox('Item',item_list,key='item_Name', on_change = update_image)
+    
+
     #Insert image of item once name is selected
-    # if item_list:
-    #     image_link = image_info[image_info['item_Name']==select_item].loc[:,'image_link'][0]
-    #     image_request = requests.get(image_link)
-    #     image = Image.open(BytesIO(image_request.content))
-    #     st.image(image, caption='Selected Item')   
+    if item_list:
+        image_link = image_info[image_info['item_Name']==select_item].loc[:,'image_Link'].tolist()[0]
+        image_request = requests.get(image_link)
+        image = Image.open(BytesIO(image_request.content))
+        st.image(image, caption='Selected Item')   
+
     #Enter the number of items to add
     enter_quantity = st.number_input('Enter quantity', min_value=0)
+
     #Add submit button
     add_item_button = st.button("Add Item")
     if add_item_button:
